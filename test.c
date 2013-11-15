87283f8a04761b0faab714160f2db55bb01f79a0
Fang Huaqi <578567190@qq.com>
2013-11-15 09:15:06 +0800

    do some fix to test.c

--- test.c
+++ test.c
@@ -2,14 +2,14 @@
 
 void printnum(int num)
 {
-    printf("Num is %d\n", num);
+    printf("The Num is %d\n", num);
 }
 
 void main(void)
 {
     int i;
 
-    for (i=0; i < 100; i++) {
+    for (i=0; i < 1000; i++) {
 	printnum(i);    
     }
 
